6415382f81e6ab074922f82abf638a9d79cab209
Randall <84122428+RandallAnjie@users.noreply.github.com>
2024-04-25 17:54:36 +0800

    更新加载图

--- plugins/vue-lazyload.ts
+++ plugins/vue-lazyload.ts
@@ -2,7 +2,6 @@ import VueLazyload from 'vue3-lazy'
 
 export default defineNuxtPlugin(nuxtApp => {
   nuxtApp.vueApp.use(VueLazyload, {
-    loading: 'path/to/loading.gif',
-    error: 'path/to/error.png',
+    loading: '/loader.svg',
   });
 });
