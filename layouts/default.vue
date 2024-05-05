533dc05b45dbd7c1d69e431be156518057dddd2c
Randall <84122428+RandallAnjie@users.noreply.github.com>
2024-05-05 09:18:05 +0800

    bug fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,7 +10,7 @@
     </ScrollArea>
   </div>
   <Toaster position="top-right" :expand="true" rich-colors />
-  <div style="position: absolute;right: 10px ;bottom: 30vh; z-index: 9999 !important;">
+  <div style="position: absolute;right: 10px ;bottom: 35vh; z-index: 9999 !important;">
     <DropdownMenuRoot v-model:open="toggleState">
       <DropdownMenuTrigger
           class="rounded-full w-[35px] h-[35px] inline-flex items-center justify-center text-grass11 bg-white shadow-[0_2px_10px] shadow-blackA7 outline-none hover:bg-green3 focus:shadow-[0_0_0_2px] focus:shadow-black"
