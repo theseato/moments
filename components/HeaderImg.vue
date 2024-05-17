ed487f87fcf4125fa8f2c7d5a9f694f49458959b
Randall <84122428+RandallAnjie@users.noreply.github.com>
2024-05-17 18:49:25 +0800

    修复个性签名位置显示bug，新增天气展示

--- components/HeaderImg.vue
+++ components/HeaderImg.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="header relative mb-8 ">
-    <img class="header-img w-full  max-h-[300px]" :src="getImgUrl(res?.data?.coverUrl!)" alt="" />
-    <div class="absolute right-2 bottom-[-40px]">
+  <div class="header relative mb-12 ">
+    <img class="header-img w-full max-h-[300px]" :src="getImgUrl(res?.data?.coverUrl!)" alt="" />
+    <div class="absolute right-2 left-2 bottom-[-40px]" style="width: calc(100% - 16px)">
       <div class="userinfo flex flex-col">
         <div class="flex flex-row items-center gap-4 justify-end">
           <div class="username text-lg font-bold text-white">{{ res?.data?.nickname }}</div>
           <img :src="getImgUrl(res?.data?.avatarUrl!)" class="avatar w-[70px] h-[70px] rounded-xl" />
         </div>
-        <div class="slogon text-gray truncate w-full text-end text-xs mt-2">{{ res?.data?.slogan }}</div>
+        <div class="flex flex-row items-center gap-4 justify-end">
+<!--          <div class="slogon text-gray truncate w-full text-end text-xs mt-2">{{ weather }}</div>-->
+          <iframe scrolling="no" src="https://widget.tianqiapi.com/?style=tz&skin=pitaya&color=000" frameborder="0" width="200" height="20" allowtransparency="true" v-if="colorMode.value === 'light'"></iframe>
+          <iframe scrolling="no" src="https://widget.tianqiapi.com/?style=tz&skin=pitaya&color=fff" frameborder="0" width="200" height="20" allowtransparency="true" v-if="colorMode.value === 'dark'"></iframe>
+          <div class="slogon text-gray truncate w-full text-end text-xs mt-2">{{ res?.data?.slogan }}</div>
+        </div>
       </div>
     </div>
   </div>
-
 </template>
 
 <script setup lang="ts">
@@ -19,7 +23,9 @@ import { settingsUpdateEvent } from '~/lib/event'
 import { getImgUrl } from '~/lib/utils';
 import {useAsyncData} from "#imports";
 import type {User} from "~/lib/types";
+const colorMode = useColorMode()
 const token = useCookie('token')
+const weather = ref('')
 const route = useRoute()
 const showBack = () => {
   return route.path.startsWith('/detail') || route.path.startsWith('/settings') || route.path.startsWith('/config')
